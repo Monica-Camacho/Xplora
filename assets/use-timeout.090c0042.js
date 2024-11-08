ff307e7ad5ec9f4b6efe9961ff266e6b8c6fb7a1
MoniCamacho <119087190+Monica-Camacho@users.noreply.github.com>
2024-11-07 21:03:06 -0600

    mensaje

---
+++ assets/use-timeout.090c0042.js
@@ -0,0 +1,1 @@
+import{b as u}from"./QBtn.a109e76d.js";import{Z as i,C as m,l as s}from"./index.ce041e64.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};

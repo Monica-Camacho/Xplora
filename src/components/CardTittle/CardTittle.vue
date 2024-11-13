2fb0318578e8a9ff382db46522f06244c8c3ab03
MoniCamacho <119087190+Monica-Camacho@users.noreply.github.com>
2024-11-13 15:53:55 -0600

    Cardtittle versión 1

---
+++ src/components/CardTittle/CardTittle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-tittle">
+    <div class="card-tittle__nombre">{{ nombre }}</div>
+    <img :src="imagen" alt="Imagen del lugar" class="card-tittle__imagen" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardTittle",
+  props: {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    imagen: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/pages/DestinosPage.vue
+++ src/pages/DestinosPage.vue
@@ -1,66 +1,59 @@
 <template>
-  <q-page
-    style="
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 20px;
-    "
-  >
+  <q-page class="pagina-destinos">
     <div class="buscar-contenedor">
       <BuscarDestino />
     </div>
 
-    <div>
+    <CardTittle
+      nombre="Nombre del lugar"
+      imagen="assets/images/tarjetadestinos/destinos.jpg"
+    />
+
+    <div class="barra-navegacion">
       <barraNavegacion />
-      <!-- Otros elementos de la página -->
     </div>
 
     <div class="contenido-principal">
-      <!-- Contenedor de tarjetas con título -->
       <h2 class="titulo-seccion">Las mejores Actividades</h2>
-    </div>
 
-    <!-- Contenedor para las tarjetas CardImage alineadas horizontalmente -->
-    <div class="card-images-container">
-      <CardImage
-        imageUrl="assets/images/tarjetadestinos/destinos.jpg"
-        activityName="Tour por la Ciudad"
-        duration="3.5 Horas - Grupo"
-        :showIcon="true"
-      />
-
-      <CardImage
-        imageUrl="assets/images/tarjetadestinos/destinos.jpg"
-        activityName="Tour por la Ciudad"
-        duration="3.5 Horas - Grupo"
-        :showIcon="false"
-      />
-
-      <CardImage
-        imageUrl="assets/images/tarjetadestinos/destinos.jpg"
-        activityName="Tour por la Ciudad"
-        duration="3.5 Horas - Grupo"
-        :showIcon="true"
-      />
-
-      <CardImage
-        imageUrl="assets/images/tarjetadestinos/destinos.jpg"
-        activityName="Tour por la Ciudad"
-        duration="3.5 Horas - Grupo"
-        :showIcon="true"
-      />
-      <CardImage
-        imageUrl="assets/images/tarjetadestinos/destinos.jpg"
-        activityName="Tour por la Ciudad"
-        duration="3.5 Horas - Grupo"
-        :showIcon="true"
-      />
+      <!-- Contenedor para las tarjetas CardImage alineadas horizontalmente -->
+      <div class="card-images-container">
+        <CardImage
+          imageUrl="assets/images/tarjetadestinos/destinos.jpg"
+          activityName="Tour por la Ciudad"
+          duration="3.5 Horas - Grupo"
+          :showIcon="true"
+        />
+        <CardImage
+          imageUrl="assets/images/tarjetadestinos/destinos.jpg"
+          activityName="Tour por la Ciudad"
+          duration="3.5 Horas - Grupo"
+          :showIcon="false"
+        />
+        <CardImage
+          imageUrl="assets/images/tarjetadestinos/destinos.jpg"
+          activityName="Tour por la Ciudad"
+          duration="3.5 Horas - Grupo"
+          :showIcon="true"
+        />
+        <CardImage
+          imageUrl="assets/images/tarjetadestinos/destinos.jpg"
+          activityName="Tour por la Ciudad"
+          duration="3.5 Horas - Grupo"
+          :showIcon="true"
+        />
+        <CardImage
+          imageUrl="assets/images/tarjetadestinos/destinos.jpg"
+          activityName="Tour por la Ciudad"
+          duration="3.5 Horas - Grupo"
+          :showIcon="true"
+        />
+      </div>
     </div>
 
     <div class="contenido-principal">
-      <!-- Contenedor de tarjetas con título -->
       <h2 class="titulo-seccion">Actividades recomendadas</h2>
+      <!-- Agrega aquí contenido adicional para actividades recomendadas si es necesario -->
     </div>
   </q-page>
 </template>
@@ -69,7 +62,8 @@
 import TarjetaDestino from "src/components/TarjetaActividades/TarjetaActividades.vue";
 import bttnGris from "src/components/bttnGris/bttnGris.vue";
 import BuscarDestino from "src/components/BuscarDestino/BuscarDestino.vue";
-import "./Destinos.scss";
+import "./Destinos.scss"; /* Importa el archivo CSS externo */
 import CardImage from "src/components/CardImage/CardImage.vue";
-import barraNavegacion from "components/barraNavegacion/barraNavegacion.vue";
+import barraNavegacion from "src/components/barraNavegacion/barraNavegacion.vue";
+import CardTittle from "src/components/CardTittle/CardTittle.vue";
 </script>

24af4d2c03980cf976eb6e3d11af161235c791ac
MoniCamacho <119087190+Monica-Camacho@users.noreply.github.com>
2024-11-15 21:11:49 -0600

    .

--- src/pages/PruebasPage.vue
+++ src/pages/PruebasPage.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="q-pa-md">
-    <q-carousel
-      swipeable
-      animated
-      v-model="slide"
-      thumbnails
-      infinite
-      arrows
-    >
+    <q-carousel swipeable animated v-model="slide" thumbnails infinite arrows>
       <!-- local -->
-      <!-- 
+      <!--
       <q-carousel-slide :name="1" img-src="/src/assets/images/foto1.jpg" />
       <q-carousel-slide :name="2" img-src="/src/assets/images/foto2.jpg" />
       <q-carousel-slide :name="3" img-src="/src/assets/images/foto3.jpg"/>
       <q-carousel-slide :name="4" img-src="/src/assets/images/foto4.jpg" />
       -->
       <!-- GitHub -->
-      <q-carousel-slide :name="1" img-src="assets/images/foto1.jpg" />
-      <q-carousel-slide :name="2" img-src="assets/images/foto2.jpg" />
-      <q-carousel-slide :name="3" img-src="assets/images/foto3.jpg"/>
-      <q-carousel-slide :name="4" img-src="assets/images/foto4.jpg" /> 
+      <q-carousel-slide
+        :name="1"
+        img-src="assets/images/tarjetadestinos/destinos.jpg"
+      />
+      <q-carousel-slide
+        :name="2"
+        img-src="assets/images/tarjetadestinos/destinos.jpg"
+      />
+      <q-carousel-slide
+        :name="3"
+        img-src="assets/images/tarjetadestinos/destinos.jpg"
+      />
+      <q-carousel-slide
+        :name="4"
+        img-src="assets/images/tarjetadestinos/destinos.jpg"
+      />
     </q-carousel>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const slide = ref(1)
+const slide = ref(1);
 </script>
-
-
-
